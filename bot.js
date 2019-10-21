656cdf526d19ad097e6fc5d8381ba772f0ffb25a
chrond <micah@chronicdev.io>
2019-10-21 14:06:25 -0500

    Write data to file in batches
    When getting message history, write the data to the file in batches instead of writing each row individually.

--- bot.js
+++ bot.js
@@ -4,18 +4,20 @@ const fs = require('fs');
 const csvFileName = 'idears.csv';
 const endOfLine = require('os').EOL;
 
+//ToDo: Keep track of oldestMessageId, oldestDate, newestMessageId, newestDate; and store them somewhere so that they persist between sessions.
+
 ComfyDiscord.onCommand = ( channelName, user, command, msg, flags, extra ) => {
   if ( command.toLowerCase() === 'getoldidears' ) {
-    if (channelName == process.env.IDEARCHANNEL) {
-      let batchSize = 10;
-      let earliestMessageId = '';
-      let earliestDate = new Date(3000, 1);
-      getMessages(extra.channel, batchSize, earliestMessageId, earliestDate, true);
+    if (channelName === process.env.IDEARCHANNEL) {
+      let batchSize = 50;
+      let oldestMessageId = '';
+      let oldestDate = new Date(3000, 1);
+      getMessages(extra.channel, batchSize, oldestMessageId, oldestDate, true);
     }
   }
   else if ( command.toLowerCase() === 'getnewidears' ) {
-    if (channelName == process.env.IDEARCHANNEL) {
-      let batchSize = 10;
+    if (channelName === process.env.IDEARCHANNEL) {
+      let batchSize = 50;
       let newestMessageId = '';
       let newestDate = new Date(1990, 1);
       getMessages(extra.channel, batchSize, newestMessageId, newestDate, false);
@@ -23,14 +25,14 @@ ComfyDiscord.onCommand = ( channelName, user, command, msg, flags, extra ) => {
   }
   else {
     //Looks like a command but should be treated as a regular comment
-    if (channelName == process.env.IDEARCHANNEL) {
-      writeToFile( extra.id, new Date(), user, '!' + command + ' ' + extra.cleanContent );
+    if (channelName === process.env.IDEARCHANNEL) {
+      writeToFile( makeCsvRowString(extra.id, new Date(), user, '!' + command + ' ' + extra.cleanContent) );
     }
   }
 }
 ComfyDiscord.onChat = ( channelName, user, msg, flags, extra ) => {
-  if (channelName == process.env.IDEARCHANNEL) {
-    writeToFile( extra.id, new Date(), user, extra.cleanContent );
+  if (channelName === process.env.IDEARCHANNEL) {
+    writeToFile( makeCsvRowString(extra.id, new Date(), user, extra.cleanContent) );
   }
 }
 ComfyDiscord.Init( process.env.DISCORDTOKEN );
@@ -38,10 +40,10 @@ ComfyDiscord.Init( process.env.DISCORDTOKEN );
 function getMessages( channel, batchSize, messageId, date, getOldMessages ) {
   let options = { limit: batchSize };
   
-  if (getOldMessages && messageId != '') {
+  if (getOldMessages && messageId !== '') {
     options.before = messageId;
   }
-  else if (!getOldMessages && messageId != '') {
+  else if (!getOldMessages && messageId !== '') {
     options.after = messageId;
   }
 
@@ -50,27 +52,38 @@ function getMessages( channel, batchSize, messageId, date, getOldMessages ) {
     .then( messages => {
       //console.log('Received ' + messages.size + ' messages.');
       
+      let dataString = '';
       for (let m of messages.values()) {
-        if ( messageId == "" || (getOldMessages && m.createdAt <= date) || (!getOldMessages && m.createdAt > date) ) {
+        if ( messageId === '' || (getOldMessages && m.createdAt <= date) || (!getOldMessages && m.createdAt > date) ) {
           messageId = m.id;
           date = m.createdAt;
         }
-        writeToFile( m.id, m.createdAt, m.author.username, m.cleanContent );
+        dataString += makeCsvRowString( m.id, m.createdAt, m.author.username, m.cleanContent );
+      }
+      if ( dataString !== '' ) {
+        // Write this batch of data to the file
+        writeToFile( dataString );
+        dataString = '';
       }
       
       //if we got a full batch, try to get more
-      if (messages.size == batchSize) {
+      if (messages.size === batchSize) {
         getMessages( channel, batchSize, messageId, date, getOldMessages );
       }
     })
     .catch(console.error);
 }
 
-function writeToFile( id, date, user, messageText ) {
+function writeToFile( dataString ) {
   //flag 'as': Open file for appending in synchronous mode. The file is created if it does not exist.
-  fs.writeFileSync(csvFileName, id + ',' + date.toISOString() + ',' + csvTextEscape(user) + ',' + csvTextEscape( messageText ) + endOfLine, { encoding: 'utf8', flag: 'as' } )
+  fs.writeFileSync(csvFileName, dataString, { encoding: 'utf8', flag: 'as' } )
 }
 
+function makeCsvRowString( id, date, user, messageText ) {
+  return id + ',' + date.toISOString() + ',' + csvTextEscape(user) + ',' + csvTextEscape( messageText ) + endOfLine
+}
+
+// Make a data string (that might have commas or doublequotes in it) ready to put into a CSV
 function csvTextEscape( str ) {
   return `"` + str.replace(/"/g, `""`).replace(/(?:\r\n|\r|\n)/g, ' ') + `"`;
 }
